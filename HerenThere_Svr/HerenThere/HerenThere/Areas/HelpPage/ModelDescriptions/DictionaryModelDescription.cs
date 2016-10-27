3b2eaa1dfb4b3e35db974f8acd25b17fcf9e8d80
Asare <opokuasarekennedy@gmail.com>
2016-10-28 01:41:06 +0300

    Reset of solution

--- HerenThere_Svr/HerenThere/HerenThere/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HerenThere.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- HerenThere_Svr/HerenThere/HerenThere/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HerenThere.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
